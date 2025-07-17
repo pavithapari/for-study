48c77a8943faf135e013497794aa4a6bc904e750
pavitha <pavitha@student.tce.edu>
2025-07-17 15:31:22 +0000

    second commit fr

--- example.py
+++ example.py
@@ -1,6 +1,7 @@
 
 def git_opeation():
 
- print("I am adding example.py file to the remote repository.")
+ print("I am adding example.py file to the remote repository. lets have some changes
+")
 
 git_opeation()
